571d8dcbbcea14a299fcbc3d3c04f98a23493118
Svyatoslav Kryukov <me@skryukov.dev>
2024-01-28 22:23:38 +0300

    Add minimal css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,78 +2,137 @@ import { useState } from "react";
 import Editor from "@monaco-editor/react";
 
 import { runWASI } from "../../engines/wasi";
+import cs from "./styles.module.css";
 
-const initialRubyCode = `require "uri/idna"
+const initialRubyCode = `require "uri-idna"
 
 URI::IDNA.register(alabel: "xn--gdkl8fhk5egc.jp", ulabel: "ハロー・ワールド.jp")
 `;
 
 export default function App() {
-  const [gem, setGem] = useState("uri-idna");
+  const [gem, setGem] = useState("");
   const [loading, setLoading] = useState(true);
   const [code, setCode] = useState(initialRubyCode);
   const [result, setResult] = useState("Press run...");
-  const [stdLog, setStdLog] = useState<string[]>([]);
-  const [errLog, setErrLog] = useState<string[]>([]);
+  const [log, setLog] = useState<string[]>([]);
+  const [editorValueSource, setEditorValueSource] = useState<"result" | "logs">("result");
+  const [installedGems, setInstalledGems] = useState<string[]>([]);
 
-  const runVM = (executeCode?: string) => {
+  const runVM = (executeCode?: string, onSuccess?: Function, onError?: Function ) => {
     setLoading(true);
-    setStdLog([]);
-    setErrLog([]);
+    setLog([]);
     setResult("");
     const setStdout = (line: string) => {
       console.log(line);
-      setStdLog((old) => [...old, line]);
+      setLog((old) => [...old, line]);
     };
     const setStderr = (line: string) => {
       console.warn(line);
-      setErrLog((old) => [...old, line]);
+      setLog((old) => [...old, `[error] ${line}`]);
     };
-    runWASI({code: (executeCode || code), setResult, setStdout, setStderr })
-      .catch(console.error)
+    runWASI({ code: (executeCode || code), setResult, setStdout, setStderr })
+      .catch((err) => {
+        setLog((old) => [...old, `[error] ${err}`]);
+        setEditorValueSource("logs");
+        onError && onError(err);
+      })
+      .then(() => {
+        setEditorValueSource("result");
+        onSuccess && onSuccess();
+      })
       .finally(() => setLoading(false));
   };
 
   const installGem = () => {
-    runVM(`Gem::Commands::InstallCommand.new.install_gem("${gem}", nil)`)
-  }
+    if (!gem) {
+      return;
+    }
+    runVM(
+      `Gem::Commands::InstallCommand.new.install_gem("${gem}", nil)`,
+      () => setInstalledGems((old) => [...old, gem]),
+    );
+    setGem("");
+  };
 
   const handleEditorChange = (value: string | undefined) => {
     setCode(value || "");
   };
 
   return (
-    <div style={{ display: "flex", padding: "10px" }}>
-      <Editor
-        height="90vh"
-        width="50%"
-        defaultLanguage="ruby"
-        defaultValue={code}
-        onChange={handleEditorChange}
-        onMount={() => setLoading(false)}
-        options={{minimap: {enabled: false}}}
-      />
+    <div className={cs.container}>
+      <div className={cs.header}>
+        <h1 className={cs.title}>Ruby WASI Playground</h1>
+      </div>
+      <div className={cs.menu}>
+        <label className={cs.menuLabel}>
+          Dependencies
+        </label>
+        <div className={cs.menuInputButton}>
+          <input
+            className={cs.menuInput}
+            onKeyDown={(event) => event.key === "Enter" && !loading && installGem()}
+            value={gem}
+            placeholder="Enter gem to install"
+            disabled={loading}
+            onChange={(event) => setGem(event.target.value)}
+          />
+          <button className={cs.menuInstallButton} disabled={loading} onClick={() => !loading && installGem()}>
+            +
+          </button>
+        </div>
+        <div className={cs.menuDependencies}>
+          {installedGems.map((gem) => (
+            <div className={cs.menuDependency} key={gem}>
+              {gem}
+            </div>
+          ))}
+        </div>
+      </div>
 
-      <div style={{ width: "50%", paddingLeft: "10px" }}>
-        <input
-          value={gem}
-          disabled={loading}
-          onChange={(event) => setGem(event.target.value)}
-        />
-        <button disabled={loading} onClick={() => !loading && installGem()}>
-          Install gem
-        </button>{" "}
+      <div className={cs.editor}>
+        <div className={cs.editorText}>
+          <Editor
+            height="100%"
+            width="100%"
+            theme="vs-dark"
+            defaultLanguage="ruby"
+            defaultValue={code}
+            onChange={handleEditorChange}
+            onMount={() => setLoading(false)}
+            options={{ wordWrap: "on", minimap: { enabled: false }, overviewRulerBorder: false }}
+          />
+        </div>
+        <div className={cs.editorFooter}>
+          <div className={cs.editorLoading}>
+            {loading && "loading..."}
+          </div>
+          <button className={cs.runButton} disabled={loading} onClick={() => !loading && runVM()}>
+            Run code
+          </button>
+        </div>
+      </div>
 
-        <button disabled={loading} onClick={() => !loading && runVM()}>
-          Run
-        </button>{" "}
-        {loading && "loading..."}
-        <h5>Result:</h5>
-        <code>{result}</code>
-        <h5>Stdout:</h5>
-        <code>{stdLog}</code>
-        <h5>Stderr:</h5>
-        <code>{errLog}</code>
+      <div className={cs.output}>
+        <div className={cs.switchButtons}>
+          <button className={`${cs.switchButton} ${editorValueSource === "result" ? cs.switchButtonActive : ""}`}
+                  onClick={() => setEditorValueSource("result")}>
+            Result
+          </button>
+          <button className={`${cs.switchButton} ${editorValueSource === "logs" ? cs.switchButtonActive : ""}`}
+                  onClick={() => setEditorValueSource("logs")}>
+            Logs
+          </button>
+        </div>
+        <div className={cs.editorText}>
+          <Editor
+            height="100%"
+            width="100%"
+            theme="vs-dark"
+            value={editorValueSource === "result" ? result : log.join("\n")}
+            language="shell"
+            options={{wordWrap: "on", lineNumbers: "off", readOnly: true, minimap: { enabled: false }, overviewRulerBorder: false, renderLineHighlight: "none" }}
+          />
+        </div>
       </div>
     </div>
   );
