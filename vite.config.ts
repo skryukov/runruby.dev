23d57367e76ad1c2c42edfcd0984ad20908a86b3
Svyatoslav Kryukov <me@skryukov.dev>
2024-01-28 17:28:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/ruby-wasi-playground/",
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
