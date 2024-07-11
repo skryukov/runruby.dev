3c70c8bc2dfa89e814c1afdb1f3623289029ece6
prog-supdex <symeton@gmail.com>
2024-07-11 14:19:05 +0200

    create folder if it doesn`t exist

--- bin/build.js
+++ bin/build.js
@@ -8,6 +8,11 @@ import { snapshot } from "@webcontainer/snapshot";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const fsDir = join(__dirname, "../public/fs");
+if (!fs.existsSync(fsDir)) {
+  fs.mkdirSync(fsDir, { recursive: true });
+}
+
 execSync("npm i && npm run build", { cwd: join(__dirname, "../packages/ruby-cli") });
 
 const stubs = await snapshot(join(__dirname, "../src/stubs"));
