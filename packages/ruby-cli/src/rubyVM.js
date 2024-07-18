35308d18c9d37211b5cf5f7986b618a699991143
prog-supdex <symeton@gmail.com>
2024-07-18 12:16:41 +0200

    little changes to rubyVM

--- packages/ruby-cli/src/rubyVM.js
+++ packages/ruby-cli/src/rubyVM.js
@@ -1,33 +1,54 @@
 import { readFileSync } from "node:fs";
-import { WASI } from "node:wasi";
 import path from "path";
-import { RubyVM } from "@ruby/wasm-wasi";
-import wasmFilePath from "../node_modules/@ruby/3.3-wasm-wasi/dist/ruby+stdlib.wasm";
+import { WASI } from "node:wasi";
+import { consolePrinter, RubyVM } from "@ruby/wasm-wasi";
+import wasmFilePath from "../node_modules/@ruby/3.3-wasm-wasi/dist/ruby.debug+stdlib.wasm";
 
-const homePath = "/home/runruby";
+const rubyStubsPath = "/gem_stubs";
+const defaultStubsPath = `${rubyStubsPath}/default_stubs.rb`;
+const rootFolder = "/home/runruby";
 
 export const initializeRuby = async (args = []) => {
-  const wasmModule = await WebAssembly.compile(readFileSync(path.resolve(__dirname, wasmFilePath)));
+  const wasmModule = await WebAssembly.compile(
+    readFileSync(path.resolve(__dirname, wasmFilePath)),
+  );
+
   const wasi = new WASI({
     version: "preview1",
     preopens: {
-      "/": `${homePath}`,
+      "/": rootFolder,
     },
     env: {
-      BUNDLE_DEFAULT_INSTALL_USES_PATH: '/bundle'
-    }
+      BUNDLE_DEFAULT_INSTALL_USES_PATH: "/bundle",
+    },
   });
 
-  const ruby = new RubyVM();
-  const imports = { wasi_snapshot_preview1: wasi.wasiImport };
-  ruby.addToImports(imports);
+  const rubyVM = new RubyVM();
+  const imports = {
+    wasi_snapshot_preview1: wasi.wasiImport,
+  };
+
+  rubyVM.addToImports(imports);
+
+  const printer = consolePrinter();
+  printer.addToImports(imports);
 
   const instance = await WebAssembly.instantiate(wasmModule, imports);
-  await ruby.setInstance(instance);
+
+  await rubyVM.setInstance(instance);
+
+  printer.setMemory(instance.exports.memory);
 
   wasi.initialize(instance);
 
-  ruby.initialize(["ruby.wasm", "-e_=0", "-EUTF-8", '-I/gem_stubs', "-rdefault_stubs", ...args]);
+  rubyVM.initialize([
+    "ruby.wasm",
+    "-e_=0",
+    "-EUTF-8",
+    `-I${rubyStubsPath}`,
+    `-r${defaultStubsPath}`,
+    ...args,
+  ]);
 
-  return ruby;
-}
+  return rubyVM;
+};
