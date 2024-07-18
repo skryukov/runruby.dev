567e9bdc0ad2365be2c345623dd99c8753ab1bef
prog-supdex <symeton@gmail.com>
2024-07-18 11:54:13 +0200

    update itty-router to v5

--- runruby-worker/src/index.ts
+++ runruby-worker/src/index.ts
@@ -1,55 +1,51 @@
-import { createCors, error, html, json, Router, withCookies } from "itty-router";
-
+import { cors, error, html, json, Router, withCookies } from "itty-router";
 import apiRouter from "./apiRouter";
 import createSession from "./withEncryptedSession";
 import compactIndexSpecs from "./compactIndexSpecs";
 import proxy from "./proxy";
 
+const dynamicOrigins = [
+  /^https:\/\/httpslocalrunrubydev5173-[a-z0-9-]+\.w-corp-staticblitz\.com$/,
+  /^https:\/\/httpslocalhost5173-[a-z0-9-]+\.w-corp-staticblitz\.com$/,
+];
+
+const { withEncryptedSession, setSessionCookie } = createSession();
+
+const { preflight, corsify } = cors({
+  allowMethods: ["GET", "PATCH", "POST"],
+  origin: (origin: string) => {
+    const staticOrigins = [
+      "http://localhost:5173",
+      "https://localhost:5173",
+      "https://runruby.dev",
+      "https://local.runruby.dev:5173",
+      "https://local.runruby.dev:5173/",
+    ];
+
+    if (staticOrigins.includes(origin) || dynamicOrigins.some((regex) => regex.test(origin))) {
+      console.log("IN THE IF CHECKING")
+      return origin;
+    }
+
+    return;
+  },
+  credentials: true,
+  allowHeaders: ["Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"],
+});
+
+const router = Router({
+  before: [withCookies, preflight, withEncryptedSession],
+  catch: error,
+  finally: [json, setSessionCookie, corsify],
+});
+
+router
+  .all("/api/*", apiRouter.handle)
+  .get("/proxy/*", proxy)
+  .get("/compact_index_specs", compactIndexSpecs)
+  .get("/", () => html(`Hello from <a href="https://runruby.dev">RunRuby.dev</a> backend lambda!`))
+  .all("*", () => new Response("Not Found.", { status: 404 }));
+
 export default {
-  fetch: (req: Request, env: Env, ctx: ExecutionContext) => {
-
-    const { withEncryptedSession, setSessionCookie } = createSession();
-    const { preflight, corsify } = createCors({
-      methods: ["GET", "PATCH", "POST"],
-      origins: (origin: string) => {
-        const hostname = new URL(origin).hostname;
-        const allowedHosts = env.ALLOWED_HOSTS.split(",").map(h => h.trim());
-        for (const allowedHost of allowedHosts) {
-          if (allowedHost.startsWith("*.")) {
-            if (hostname.endsWith(allowedHost.slice(1))) {
-              return true;
-            }
-          } else if (hostname === allowedHost) {
-            return true;
-          }
-        }
-        return false;
-      },
-      headers: {
-        "Access-Control-Allow-Credentials": "true"
-      }
-    });
-
-    const router = Router();
-
-    router
-      .all("*", withCookies)
-      .all("*", preflight)
-      .all("*", withEncryptedSession)
-      .all("/api/*", apiRouter.handle)
-      .get("/proxy/*", proxy)
-      .get("/compact_index_specs", compactIndexSpecs)
-      .get("/", () => html(
-        `Hello from <a href="https://runruby.dev">RunRuby.dev</a> backend lambda!`
-      ))
-      .all("*", () => new Response("Not Found.", { status: 404 }));
-
-    return router
-      .handle(req, env, ctx)
-      .then(json)
-      .catch(error)
-      .then(setSessionCookie)
-      .then(corsify)
-  }
+  fetch: router.fetch,
 };
-
